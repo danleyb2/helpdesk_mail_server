75b7019929104f1a49493bc1a0de4b5d3baae392
nyaundi brian <danleyb2@gmail.com>
2019-02-10 13:01:37 +0300

    sent test mail - add message body

--- send_test_mail.sh
+++ send_test_mail.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-swaks -h domain.com -f danleyb2@gmail.com -t me@danleyb2.online -s localhost -p 25
+swaks --body test_email.html --add-header "MIME-Version: 1.0" --add-header "Content-Type: text/html"  -h domain.com -f danleyb2@gmail.com -t support@danleyb2.online -s localhost -p 2525
